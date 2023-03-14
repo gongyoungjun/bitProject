246fd48a84421de88bb0665544c1e702ae591a2d
gongyoungjun <117051273+gongyoungjun@users.noreply.github.com>
2023-03-14 10:06:52 +0900

    Update PlayService.java

--- sample/src/main/java/com/bit/web/play/service/PlayService.java
+++ sample/src/main/java/com/bit/web/play/service/PlayService.java
@@ -7,6 +7,6 @@ public interface PlayService {
 	
     String find_user_id(String email);
 
-    void insertSeqNumber(membersBean bean); 
+    void insertSeqNumber(membersBean bean);  // static으로 선언을 하게 되면 에러가 발생합니다.
 
 }
